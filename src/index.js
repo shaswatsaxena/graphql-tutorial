38b6788a296f9d492549af8fb05e5b8d1cf18593
shaswatsaxena <shaswatsaxena@gmail.com>
2019-05-13 15:42:28 +0530

    Completed

--- src/index.js
+++ src/index.js
@@ -1,40 +1,56 @@
 const { prisma } = require("./generated/prisma-client");
 const { GraphQLServer } = require("graphql-yoga");
 
+// const resolvers = {
+//   Query: {
+//     info: () => `This is the API of a Hackernews Clone`,
+//     feed: (root, args, context, info) => {
+//       return context.prisma.links();
+//     },
+//     link: (parent, args) => {
+//       const link = links.find(linkObj => linkObj.id === args.id);
+//       return link;
+//     }
+//   },
+//   Mutation: {
+//     post: (root, args, context) => {
+//       return context.prisma.createLink({
+//         url: args.url,
+//         description: args.description
+//       });
+//     },
+//     updateLink: (parent, args) => {
+//       const updatedLinkIndex = links.findIndex(
+//         linkObj => linkObj.id === args.id
+//       );
+//       links[updatedLinkIndex].url = args.url;
+//       links[updatedLinkIndex].description = args.description;
+//       return links[updatedLinkIndex];
+//     },
+//     deleteLink: (parent, args) => {
+//       const deleteLinkIndex = links.findIndex(
+//         linkObj => linkObj.id === args.id
+//       );
+//       const deletedLink = links.splice(deleteLinkIndex, 1);
+//       return deletedLink[0];
+//     }
+//   }
+// };
+
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const Subscription = require("./resolvers/Subscription");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Vote = require("./resolvers/Vote");
+
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      return context.prisma.links();
-    },
-    link: (parent, args) => {
-      const link = links.find(linkObj => linkObj.id === args.id);
-      return link;
-    }
-  },
-  Mutation: {
-    post: (root, args, context) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description
-      });
-    },
-    updateLink: (parent, args) => {
-      const updatedLinkIndex = links.findIndex(
-        linkObj => linkObj.id === args.id
-      );
-      links[updatedLinkIndex].url = args.url;
-      links[updatedLinkIndex].description = args.description;
-      return links[updatedLinkIndex];
-    },
-    deleteLink: (parent, args) => {
-      const deleteLinkIndex = links.findIndex(
-        linkObj => linkObj.id === args.id
-      );
-      const deletedLink = links.splice(deleteLinkIndex, 1);
-      return deletedLink[0];
-    }
-  }
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote
 };
 
 const server = new GraphQLServer({
